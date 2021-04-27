0e421b94b5d9ba1f6ac5167c39743303b473e218
D3krish128 <83149128+D3krish128@users.noreply.github.com>
2021-04-27 22:24:25 +0530

    Delete amaansh1vamrun.py

--- amaansh1vamrun.py
+++
@@ -1,4 +0,0 @@
-#To solbe suspension issue #By sh1vam
-import os
-os.system("git clone https://github.com/kuldeepsinghzala880/BLACK-GHOULS /root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && chmod +x /usr/local/bin/* && python3 -m userbot")
-© 2021 GitHub, Inc.
